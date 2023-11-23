1ced45217a9c32ed1b96cf83d6f9d3700f63824a
Yehor <egorkorsun7@gmail.com>
2023-11-23 20:37:17 +0100

    added get for server status

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000");
+        });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+
+app.UseCors();
+
+app.MapGet("/", () => "hello world!!");
+app.MapGet("/isonline", () => true);
 
 app.Run();
