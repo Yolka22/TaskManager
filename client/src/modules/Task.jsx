06715a4724cc2287cf09b9b416b53eb1c88e0a78
Yehor <egorkorsun7@gmail.com>
2023-11-25 17:00:02 +0100

    task info display added

--- client/src/modules/Task.jsx
+++ client/src/modules/Task.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import {React ,useState, Fragment} from "react";
 import Button from "@mui/joy/Button";
 import Modal from "@mui/joy/Modal";
 import ModalClose from "@mui/joy/ModalClose";
@@ -7,9 +7,9 @@ import Sheet from "@mui/joy/Sheet";
 import { Box, Typography } from "@mui/joy";
 
 export default function Task({ task }) {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   return (
-    <React.Fragment>
+    <Fragment>
       <Button
         sx={{
           height: "250px",
@@ -19,10 +19,12 @@ export default function Task({ task }) {
         color="neutral"
         onClick={() => setOpen(true)}
       >
-        <Box sx={{
-            display:"flex",
-            flexDirection:"column",
-        }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
           <Typography>{task.title}</Typography>
           <Typography>{task.id}</Typography>
         </Box>
@@ -37,6 +39,7 @@ export default function Task({ task }) {
         <Sheet
           variant="outlined"
           sx={{
+            width: 400,
             maxWidth: 500,
             borderRadius: "md",
             p: 3,
@@ -44,9 +47,29 @@ export default function Task({ task }) {
           }}
         >
           <ModalClose variant="plain" sx={{ m: 1 }} />
-          <Typography>Modal</Typography>
+          <Typography level="h2" fontSize="xl">
+            {task.title}
+          </Typography>
+          <Typography level="h2" fontSize="xl">
+            {task.description}
+          </Typography>
+          <Typography level="h2" fontSize="xl">
+            Sub Tasks
+          </Typography>
+          {task.subtasks == null ? (
+            <Box sx={{ width: 400, height: 40 }}>No Subtasks</Box>
+          ) : (
+            <Box>
+              {task.subtasks.map((subtask) => {
+                return <Typography>{subtask.title}</Typography>;
+              })}
+            </Box>
+          )}
+          <Typography level="h2" fontSize="xl">
+            {task.deadline}
+          </Typography>
         </Sheet>
       </Modal>
-    </React.Fragment>
+    </Fragment>
   );
 }
