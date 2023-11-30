10e31d14eb4b3b7f3cea96380fcfb1ec5ae0e1a6
Yehor <egorkorsun7@gmail.com>
2023-11-30 15:55:14 +0100

    reload app crush fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
-//index.js
+// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
 import { Provider } from "react-redux";
-import store from "./redux/store";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store";
 import HomePage from "./modules/pages/HomePage";
 import LoginForm from "./modules/forms/LoginForm";
 import RegisterForm from "./modules/forms/RegisterForm";
@@ -16,13 +14,15 @@ import RegisterForm from "./modules/forms/RegisterForm";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </PersistGate>
   </Provider>
 );
 
